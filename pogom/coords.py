eaf673eb01072f32d81bfab4cb7ad850688903a8
Valian <jakub.skalecki@gmail.com>
2016-07-24 20:13:29 +0200

    search.py PEP8 code improvements

--- pogom/coords.py
+++ pogom/coords.py
@@ -1,3 +1,15 @@
+import math
+
+# Constants for Hex Grid
+# Gap between vertical and horzonal "rows"
+lat_gap_meters = 150
+lng_gap_meters = 86.6
+
+# 111111m is approx 1 degree Lat, which is close enough for this
+meters_per_degree = 111111
+lat_gap_degrees = float(lat_gap_meters) / meters_per_degree
+
+
 def get_direction_string(origin, target):
     diff = target - origin
     direction = ''
@@ -12,3 +24,8 @@ def get_direction_string(origin, target):
 def get_distance(origin, target):
     radians_to_meters = 6366468.241830914
     return origin.get_distance(target).radians * radians_to_meters
+
+
+def calculate_lng_degrees(latitude):
+    degrees = math.cos(math.radians(latitude))
+    return float(lng_gap_meters) / (meters_per_degree * degrees)
--- pogom/search.py
+++ pogom/search.py
@@ -1,46 +1,34 @@
-#!/usr/bin/python
+# !/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import logging
 import time
-import math
-
 from threading import Thread, Lock
+
 from queue import Queue
 
 from pgoapi import PGoApi
 from pgoapi.utilities import f2i, get_cellid
-
-from . import config
-from .models import parse_map
+from pogom import config
+from pogom.coords import calculate_lng_degrees, lat_gap_degrees
+from pogom.models import parse_map
 
 log = logging.getLogger(__name__)
 
-TIMESTAMP = '\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000'
+TIMESTAMP = '\000' * 21
 api = PGoApi()
 
-#Constants for Hex Grid
-#Gap between vertical and horzonal "rows"
-lat_gap_meters = 150
-lng_gap_meters = 86.6
-
-#111111m is approx 1 degree Lat, which is close enough for this
-meters_per_degree = 111111
-lat_gap_degrees = float(lat_gap_meters) / meters_per_degree
-
 search_queue = Queue(config['SEARCH_QUEUE_DEPTH'])
-
-def calculate_lng_degrees(lat):
-    return float(lng_gap_meters) / (meters_per_degree * math.cos(math.radians(lat)))
 
 
 def send_map_request(api, position):
     try:
         api.set_position(*position)
-        api.get_map_objects(latitude=f2i(position[0]),
-                            longitude=f2i(position[1]),
-                            since_timestamp_ms=TIMESTAMP,
-                            cell_id=get_cellid(position[0], position[1]))
+        api.get_map_objects(
+            latitude=f2i(position[0]),
+            longitude=f2i(position[1]),
+            since_timestamp_ms=TIMESTAMP,
+            cell_id=get_cellid(position[0], position[1]))
         return api.call()
     except Exception as e:
         log.warning("Uncaught exception when downloading map " + str(e))
@@ -49,43 +37,44 @@ def send_map_request(api, position):
 
 def generate_location_steps(initial_location, num_steps):
 
-    ring = 1 #Which ring are we on, 0 = center
+    ring = 1  # Which ring are we on, 0 = center
     lat_location = initial_location[0]
     lng_location = initial_location[1]
 
-    yield (initial_location[0],initial_location[1], 0) #Middle circle
+    yield (initial_location[0], initial_location[1], 0)  # Middle circle
 
     while ring < num_steps:
-        #Move the location diagonally to top left spot, then start the circle which will end up back here for the next ring
-        #Move Lat north first
+        # Move the location diagonally to top left spot, then start
+        # the circle which will end up back here for the next ring
+        # Move Lat north first
         lat_location += lat_gap_degrees
         lng_location -= calculate_lng_degrees(lat_location)
 
         for direction in range(6):
             for i in range(ring):
-                if direction == 0: #Right
+                if direction == 0:  # Right
                     lng_location += calculate_lng_degrees(lat_location) * 2
 
-                if direction == 1: #Right Down
+                if direction == 1:  # Right Down
                     lat_location -= lat_gap_degrees
                     lng_location += calculate_lng_degrees(lat_location)
 
-                if direction == 2: #Left Down
+                if direction == 2:  # Left Down
                     lat_location -= lat_gap_degrees
                     lng_location -= calculate_lng_degrees(lat_location)
 
-                if direction == 3: #Left
+                if direction == 3:  # Left
                     lng_location -= calculate_lng_degrees(lat_location) * 2
 
-                if direction == 4: #Left Up
+                if direction == 4:  # Left Up
                     lat_location += lat_gap_degrees
                     lng_location -= calculate_lng_degrees(lat_location)
 
-                if direction == 5: #Right Up
+                if direction == 5:  # Right Up
                     lat_location += lat_gap_degrees
                     lng_location += calculate_lng_degrees(lat_location)
 
-                yield (lat_location, lng_location, 0) #Middle circle
+                yield (lat_location, lng_location, 0)  # Middle circle
 
         ring += 1
 
@@ -101,13 +90,16 @@ def login(args, position):
 
     log.info('Login to Pokemon Go successful.')
 
-def create_search_threads(num) :
+
+def create_search_threads(num):
     search_threads = []
     for i in range(num):
-        t = Thread(target=search_thread, name='search_thread {}'.format(i), args=( search_queue,))
+        name = 'search_thread {}'.format(i)
+        t = Thread(target=search_thread, name=name, args=(search_queue,))
         t.daemon = True
         t.start()
         search_threads.append(t)
+
 
 def search_thread(args):
     queue = args
@@ -123,10 +115,14 @@ def search_thread(args):
                     try:
                         parse_map(response_dict, i, step, step_location)
                     except KeyError:
-                        log.error('Scan step {:d} failed. Response dictionary key error.'.format(step))
+                        log.error(
+                            'Scan step {:d} failed. Response '
+                            'dictionary key error.'.format(step))
                         failed_consecutive += 1
-                        if(failed_consecutive >= config['REQ_MAX_FAILED']):
-                            log.error('Niantic servers under heavy load. Waiting before trying again')
+                        if failed_consecutive >= config['REQ_MAX_FAILED']:
+                            log.error(
+                                'Niantic servers under heavy load. '
+                                'Waiting before trying again')
                             time.sleep(config['REQ_HEAVY_SLEEP'])
                             failed_consecutive = 0
                         response_dict = {}
@@ -135,14 +131,17 @@ def search_thread(args):
 
         time.sleep(config['REQ_SLEEP'])
 
+
 def process_search_threads(search_threads, curr_steps, total_steps):
     for thread in search_threads:
         thread.start()
     for thread in search_threads:
         curr_steps += 1
         thread.join()
-        log.info('Completed {:5.2f}% of scan.'.format(float(curr_steps) / total_steps*100))
+        log.info('Completed {:5.2f}% of scan.'.format(
+            float(curr_steps) / total_steps * 100))
     return curr_steps
+
 
 def search(args, i):
     num_steps = args.step_limit
@@ -150,10 +149,12 @@ def search(args, i):
     position = (config['ORIGINAL_LATITUDE'], config['ORIGINAL_LONGITUDE'], 0)
 
     if api._auth_provider and api._auth_provider._ticket_expire:
-        remaining_time = api._auth_provider._ticket_expire/1000 - time.time()
+        remaining_time = api._auth_provider._ticket_expire / 1000 - time.time()
 
         if remaining_time > 60:
-            log.info("Skipping Pokemon Go login process since already logged in for another {:.2f} seconds".format(remaining_time))
+            log.info(
+                "Skipping Pokemon Go login process since already logged in "
+                "for another {:.2f} seconds".format(remaining_time))
         else:
             login(args, position)
     else:
@@ -161,11 +162,8 @@ def search(args, i):
 
     lock = Lock()
 
-    search_threads = []
-    curr_steps = 0
-    max_threads = args.num_threads
-
-    for step, step_location in enumerate(generate_location_steps(position, num_steps), 1):
+    locations = enumerate(generate_location_steps(position, num_steps), 1)
+    for step, step_location in locations:
         if 'NEXT_LOCATION' in config:
             log.info('New location found. Starting new scan.')
             config['ORIGINAL_LATITUDE'] = config['NEXT_LOCATION']['lat']
@@ -174,8 +172,9 @@ def search(args, i):
             search(args, i)
             return
 
-        search_args = ( i, total_steps, step_location, step, lock)
+        search_args = (i, total_steps, step_location, step, lock)
         search_queue.put(search_args)
+
 
 def search_loop(args):
     i = 0
@@ -185,12 +184,16 @@ def search_loop(args):
             search(args, i)
             log.info("Scanning complete.")
             if args.scan_delay > 1:
-                log.info('Waiting {:d} seconds before beginning new scan.'.format(args.scan_delay))
+                log.info(
+                    'Waiting {:d} seconds before beginning '
+                    'new scan.'.format(args.scan_delay))
                 time.sleep(args.scan_delay)
             i += 1
 
     # This seems appropriate
-    except Exception as e:
-        log.info('Crashed, waiting {:d} seconds before restarting search.'.format(args.scan_delay))
+    except Exception:
+        log.info(
+            'Crashed, waiting {:d} seconds before '
+            'restarting search.'.format(args.scan_delay))
         time.sleep(args.scan_delay)
         search_loop(args)
